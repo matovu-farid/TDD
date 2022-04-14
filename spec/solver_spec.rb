851d29b82b897ea0f1f44f845495060810ed76c5
matovu-farid <matovu90@gmail.com>
2022-04-14 16:01:08 +0300

    Add logic for the special case

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,5 +11,9 @@ describe Solver do
     it 'should raise an exception with a negative integer' do
       expect { @solver.factorial(-1) }.to raise_error('Number can not be negative')
     end
+    it 'should return 1 if a 0 is passed as an argument' do
+      actual_factorial = @solver.factorial(0)
+      expect(actual_factorial).to eq 1
+    end
   end
 end
