69ed402f6fe45c706308747e32f10311aa344f35
matovu-farid <matovu90@gmail.com>
2022-04-14 15:25:26 +0300

    Add the test for the factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,4 +3,10 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
+  describe 'factorial' do
+    it 'should take an integer and return the factorial' do
+      actual_factorial = @solver.factorial(3)
+      expect(actual_factorial).to eq 6
+    end
+  end
 end
