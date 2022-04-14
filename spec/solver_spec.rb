0511af483e8ab53a5a59ff3b72917bf83377530f
matovu-farid <matovu90@gmail.com>
2022-04-14 15:41:37 +0300

    test to check whether an exception is raised when a negative is passed to factial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,5 +8,9 @@ describe Solver do
       actual_factorial = @solver.factorial(3)
       expect(actual_factorial).to eq 6
     end
+    it 'should raise an exception with a negative integer' do
+      actual_factorial = @solver.factorial(-1)
+      expect { actual_factorial }.to raise_error('Number can not be negative')
+    end
   end
 end
